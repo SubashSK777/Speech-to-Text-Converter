05b1dad3dbe9d4fe0cf62e4f24cf0b320939f0a2
Subash Kumar <150102220+SubashSK777@users.noreply.github.com>
2024-07-30 07:30:07 +0530

    Updated

--- script.js
+++ script.js
@@ -1,71 +1,50 @@
+const resultElement = document.getElementById("result");
+let recognition;
 
-const resultElement = document.getElementById("result")
-let recognitiion;
-
-
+document.addEventListener("DOMContentLoaded", function() {
+    if ('SpeechRecognition' in window || 'webkitSpeechRecognition' in window) {
+        console.log('Your browser supports SpeechRecognition.');
+        const SpeechRecognition = window.SpeechRecognition || window.webkitSpeechRecognition;
+        recognition = new SpeechRecognition();
+        setupRecognition(recognition);
+    } else {
+        alert('Your browser does not support speech recognition.');
+    }
+});
 
 function startConverting(){
-
-    if('webkitSpeechRecognition' in window){
-
-        recognitiion = new webkitSpeechRecognition();
-
-        setupRecognition(recognitiion);
-
-        recognitiion.start();
-        
+    if(recognition){
+        recognition.start();
     }
-
-
 }
 
-function setupRecognition(recognitiion){
-
-    recognitiion.continuous = true;
-
-    recognitiion.interimResults = true;
-
-    recognitiion.lang = 'en-US';
-
-    recognitiion.onresult = function(event){
-
-        const {finalTranscript, interTranscript} =
-        processResult(event.results);
+function setupRecognition(recognition){
+    recognition.continuous = true;
+    recognition.interimResults = true;
+    recognition.lang = 'en-US';
+    recognition.onresult = function(event){
+        const { finalTranscript, interTranscript } = processResult(event.results);
         resultElement.innerHTML = finalTranscript + interTranscript;
-    }
-
+    };
 }
 
 function processResult(results){
-
     let finalTranscript = '';
     let interTranscript = '';
-
     for(let i = 0; i < results.length; i++){
-
-        let transcript = results[i] [0].transcript;
-        transcript.replace("\n", "<br>");
-
+        let transcript = results[i][0].transcript;
+        transcript = transcript.replace("\n", "<br>");
         if(results[i].isFinal){
-
             finalTranscript += transcript;
-
-        }else{
-
+        } else {
             interTranscript += transcript;
-
         }
     }
-
-    return {finalTranscript, interTranscript}
-
-
+    return { finalTranscript, interTranscript };
 }
 
 function stopConverting(){
-
-    if(recognitiion){
-        recognitiion.stop();
+    if(recognition){
+        recognition.stop();
     }
-
 }
